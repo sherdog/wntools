a3a03097b6d4d0173c4a1843710073571245be8b
Mike Sheridan <sherdog@gmail.com>
2015-07-04 10:04:02 -0500

    added production config

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/wntools/';
+$config['base_url'] = 'https://wntools.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
