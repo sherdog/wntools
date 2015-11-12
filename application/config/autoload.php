f3ce97d086b478521e6337db55ab7f6be063ff48
Mike Sheridan <sherdog@gmail.com>
2015-11-11 23:06:02 -0600

    fdfsdf

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('ion_auth', 'mongo_dbb', 'events');
+$autoload['libraries'] = array('ion_auth', 'mongo_dbb');
 
 
 /*
